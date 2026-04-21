1d41a7a3acdd240c5ab1ff02ccc6924f3e44e39b
Sukanya Rao <srao45690@gmail.com>
2026-04-21 14:32:02 +0530

    Add SSL for Railway DB

--- db.js
+++ db.js
@@ -1,17 +1,25 @@
-const mysql = require("mysql2");
 require("dotenv").config();
+const mysql = require("mysql2");
+
+console.log("DB_HOST =", process.env.DB_HOST);
+console.log("DB_USER =", process.env.DB_USER);
+console.log("DB_NAME =", process.env.DB_NAME);
+console.log("DB_PORT =", process.env.DB_PORT);
 
 const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT || 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect((err) => {
   if (err) {
-    console.log("DB ERROR ❌", err.message);
+    console.log("DB ERROR ❌", err);
   } else {
     console.log("DB CONNECTED ✅");
   }
